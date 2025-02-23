8e582a9871bb1849ee5b76ef4cf1371ced2932be
Chicuno <computacionyoficina@hotmail.com>
2025-02-23 00:35:16 -0700

    Se creó la función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+var amigos = []
+var amigo = 0
+
+function agregarAmigo() {
+    amigo = document.getElementById('amigo').value;
+    if( amigo == 0) {
+        alert("Por favor inserta un nombre");
+    }else{
+        amigos.push (document.getElementById('amigo').value);
+        document.getElementById('amigo').value = "";
+        desplegarLista();
+        return;             
+    }    
+}
